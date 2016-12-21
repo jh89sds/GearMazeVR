686e4014e9a75330c28104d6418ef4743ae0a656
Jason <sryu99@gmail.com>
2016-12-21 18:26:45 +0900

    add clear animation

--- Assets/ClearTrigger.cs
+++ Assets/ClearTrigger.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class ClearTrigger : MonoBehaviour {
 	public AudioSource audioSource;
+	public ParticleSystem particleSystem;
+	public GameObject goalIcon;
+	bool isMovingToClear = false;
+	float fTime = 0f;
+
+	AsyncOperation asyncOper;
 
 	public void Awake() {
 		audioSource = GetComponent<AudioSource> ();
 	}
 
 	public void OnTriggerEnter(Collider other) {
-		audioSource.Play ();
-		Application.LoadLevel ("ClearMaze");
+		StartCoroutine(StartLoad("ClearMaze"));
+	}
+
+	void Update () {
+		if (isMovingToClear) {
+			fTime += Time.deltaTime;
+
+			if (fTime >= 2)
+			{
+				asyncOper.allowSceneActivation = true;
+			}
+		}
+	}
+
+
+	public IEnumerator StartLoad(string strSceneName)
+	{
+		asyncOper = Application.LoadLevelAsync(strSceneName);
+		asyncOper.allowSceneActivation = false;
+
+		if (isMovingToClear == false)
+		{
+			isMovingToClear = true;
+
+			audioSource.Play ();
+			goalIcon.SetActive (false);
+			particleSystem.Play ();
+
+			while (asyncOper.progress < 0.9f)
+			{
+				yield return true;
+			}
+		}
 	}
 }
