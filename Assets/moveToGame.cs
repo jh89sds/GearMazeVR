662d4e3e5072541b5995a5591d5b4bc03dc6b839
Jason <sryu99@gmail.com>
2016-12-21 11:34:37 +0900

    add start scene

---
+++ Assets/moveToGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveToGame : MonoBehaviour {
+	int button=0;
+	bool isMovingToGame=false;
+
+	void Update () {
+		if (Input.GetMouseButtonDown (button) && isMovingToGame == false) {
+			isMovingToGame = true;
+			Application.LoadLevel ("GearMaze");
+		}
+	}
+}
